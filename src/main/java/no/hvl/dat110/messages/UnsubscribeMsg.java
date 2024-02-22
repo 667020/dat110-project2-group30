d5845ca8c958e7e340894cad51f80cbfb437a64e
h602408 <602408@stud.hvl.no>
2024-02-22 11:19:19 +0100

    gjordt Subscribe og Unsubscribe

--- src/main/java/no/hvl/dat110/messages/UnsubscribeMsg.java
+++ src/main/java/no/hvl/dat110/messages/UnsubscribeMsg.java
@@ -2,13 +2,26 @@ package no.hvl.dat110.messages;
 
 public class UnsubscribeMsg extends Message {
 
-	// message sent from client to unsubscribe on a topic 
+	// message sent from client to unsubscribe on a topic
+    private String topic;
 
     public UnsubscribeMsg(String user, String topic) {
+        super(MessageType.UNSUBSCRIBE,user);
+        this.topic = topic;
 
     }
 
-	// TODO:
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+    public String toString() {
+        return "UnsubcribeMsg [topic=" + topic + ", user=" + getUser() + "]";
+    }
+    // TODO:
 	// Implement object variables - a topic is required
 
 	// Complete the constructor, get/set-methods, and toString method
